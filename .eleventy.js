8f46ca53268483912a0573e4b4596f86b32a24a1
Angelique Weger <hello@angeliqueweger.com>
2020-01-05 17:59:11 -0500

    Add tagcount JS

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addCollection("tagList", require("./src/_11ty/getTagList"));
+  eleventyConfig.addCollection("tagCount", require("./src/_11ty/getTagCount"));
 
   eleventyConfig.addCollection("posts", function (collection) {
     const coll = collection.getFilteredByTag("posts");
