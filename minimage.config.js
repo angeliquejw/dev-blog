7bdcaadfcb30ba4cc24d7378363c1c2c446993fd
Angelique Weger <angelique.weger@fracturedatlas.org>
2020-01-09 09:25:33 -0500

    Add minimage to compress images and svgs

---
+++ minimage.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    tinypng_key: 'HdFK2dtMXImbRppPXxoeJHvMpKEY8ONS',
+
+    paths: [
+        'src/assets/img/*.{png,jpg,gif,svg}',
+        'src/assets/img/blog/2015/*.{png,jpg,gif,svg}',
+        'src/assets/img/blog/2020/*.{png,jpg,gif,svg}',
+    ],
+
+    exclusions: [],
+
+}
