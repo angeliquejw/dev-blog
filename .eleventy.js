ad8e4be2239086d2d8e1218702c73d125fe6349f
Angelique Weger <angelique.weger@fracturedatlas.org>
2020-01-11 11:15:19 -0500

    Remove eleventynav plugin and use manual page list

--- .eleventy.js
+++ .eleventy.js
@@ -4,12 +4,10 @@ const {
 const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
 
   eleventyConfig.setDataDeepMerge(true);
 
