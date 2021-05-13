5c52055143f06fa449ba46d1e4f5e5c847b2d445
Angelique Weger <hello@angeliqueweger.com>
2021-05-13 10:44:43 -0400

    Optimize images

--- minimage.config.js
+++ minimage.config.js
@@ -1,13 +1,12 @@
 module.exports = {
+	tinypng_key: "HdFK2dtMXImbRppPXxoeJHvMpKEY8ONS",
 
-    tinypng_key: 'HdFK2dtMXImbRppPXxoeJHvMpKEY8ONS',
+	paths: [
+		"src/assets/img/*.{png,jpg,gif,svg}",
+		"src/assets/img/blog/2015/*.{png,jpg,gif,svg}",
+		"src/assets/img/blog/2020/*.{png,jpg,gif,svg}",
+		"src/assets/img/blog/2021/*.{png,jpg,gif,svg}",
+	],
 
-    paths: [
-        'src/assets/img/*.{png,jpg,gif,svg}',
-        'src/assets/img/blog/2015/*.{png,jpg,gif,svg}',
-        'src/assets/img/blog/2020/*.{png,jpg,gif,svg}',
-    ],
-
-    exclusions: [],
-
-}
+	exclusions: [],
+};
