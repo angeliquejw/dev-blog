40546a3d89773a310acd4a868a4073a6bdd4338d
Angelique Weger <hello@angeliqueweger.com>
2022-01-04 21:51:34 -0500

    Run minimage

--- minimage.config.js
+++ minimage.config.js
@@ -6,7 +6,19 @@ module.exports = {
 		"src/assets/img/blog/2015/*.{png,jpg,jpeg,gif,svg}",
 		"src/assets/img/blog/2020/*.{png,jpg,jpeg,gif,svg}",
 		"src/assets/img/blog/2021/*.{png,jpg,jpeg,gif,svg}",
+		"src/assets/img/blog/2022/*.{png,jpg,jpeg,gif,svg}",
 	],
 
-	exclusions: [],
+	exclusions: [
+		"src/assets/img/heart-black.svg",
+		"src/assets/img/icon-email.svg",
+		"src/assets/img/logo-github.svg",
+		"src/assets/img/logo-linkedin.svg",
+		"src/assets/img/logo-twitter.svg",
+		"src/assets/img/triangle-black.svg",
+		"src/assets/img/triangle-white.svg",
+		"src/assets/img/heart-black.svg",
+		"src/assets/img/scribble.svg",
+		"src/assets/img/square.svg",
+	],
 };
