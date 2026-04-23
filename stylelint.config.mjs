148323cb4c700b732027a58ca9d123f95ca30f0b
Angelique Weger <hello@angeliqueweger.com>
2026-04-22 23:33:25 -0400

    Update stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("stylelint").Config} */
+export default {
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-8-point-grid"
+  ],
+  "plugins": [
+    "stylelint-order"
+  ],
+};
